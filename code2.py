edaff1ff63dfebd16ca6553a7ab221bbf1f5950a
Nikhita03 <51832857+Nikhita03@users.noreply.github.com>
2019-06-20 10:08:25 +0530

    Update and rename trail.py to code2.py

--- code2.py
+++ code2.py
@@ -1,13 +1,16 @@
+#CODE FOR SINGLE PAGE WEBSITE
 from urllib.request import urlopen as ureq
 from bs4 import BeautifulSoup as soup
 
-my_url = "http://www.apsec.gov.in/ELECTIONRESULTS/RESULTS%202014/Andhra%20Elected%20Corporators%20List,%202014.pdf"
+#INSERT MAIN URL IN  HERE ""
+my_url = ""
 
 uClient = ureq(my_url)
 page_html = uClient.read()
 uClient.close()
 page_soup = soup(page_html, "html.parser")
-containers = page_soup.findAll("div", ("canvasWrapper"))
+containers = page_soup.findAll("div", (""))
+#INSERT DIV CLASS ABOVE IN ""
 
 print(len(containers))
 
@@ -21,8 +24,9 @@ f = open(filename, "w")
 headers = "ward\n"
 f.write(headers)
 
+#INSERT SUB DIV CLASS IN HERE ""
 for container in containers:
-    ward_container = container.findAll("div", ("textLayer"))
+    ward_container = container.findAll("div", (""))
     ward = ward_container[0].text
 
     print("ward:" + ward)
