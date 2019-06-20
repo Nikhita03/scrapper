6b64952fec1c55073bec901f519f48ba14734760
Nikhita03 <51832857+Nikhita03@users.noreply.github.com>
2019-06-20 10:04:24 +0530

    Update bbmp.py

--- bbmp.py
+++ bbmp.py
@@ -1,6 +1,8 @@
+#CODE FOR SCRAPING MANY PAGES ON SINGLE WEBSITE
 from urllib.request import urlopen as ureq
 from bs4 import BeautifulSoup as soup
 
+#INSERT DIV CLASS HERE""
 def getContents(my_url):
     uClient = ureq(my_url)
     page_html = uClient.read()
@@ -18,11 +20,12 @@ def getContents(my_url):
     headers = "ward\n"
     f.write(headers)
 
+    #INSERT SUB DIV CLASS HERE ""
     for container in containers:
         ward_container = container.findAll("div", (""))
         ward = ward_container[0].text.strip()
         print("ward" + ward)
-
+#INSERT MAIN URL HERE ""
 for x in range(1, 4):
    my_url = ""
    getContents(my_url)
